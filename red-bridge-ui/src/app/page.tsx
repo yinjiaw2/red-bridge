388882d87286dc85893a8c20cbe4ea4313e250a3
nicole-nxn <nicolenxn1234@gmail.com>
2026-04-13 17:55:07 +1000

    fix error

--- red-bridge-ui/src/app/page.tsx
+++ red-bridge-ui/src/app/page.tsx
@@ -26,7 +26,6 @@ export default function Home() {
         <CaseStudySection />
         <ProcessSection />
         <ProgramsSection />
-        <StatsSection />
         <FAQSection />
         <Footer />
       </main>
