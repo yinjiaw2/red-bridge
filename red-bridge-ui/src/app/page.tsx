baf31932849a5a929fe07ef48151aa52105dc539
nicole-nxn <nicolenxn1234@gmail.com>
2026-04-14 12:19:24 +1000

    fix home page

--- red-bridge-ui/src/app/page.tsx
+++ red-bridge-ui/src/app/page.tsx
@@ -4,13 +4,8 @@ import UserScenarioSection from "@/components/home/UserScenarioSection";
 import TrustSafetySection from "@/components/home/TrustSafetySection";
 import SuccessTicker from "@/components/home/OutcomeTickerSection";
 import ComparisonSection from "@/components/home/ComparisonSection";
-import ProcessSection from "@/components/home/ProcessSection";
-import ProgramsSection from "@/components/home/ProgramsSection";
-import StatsSection from "@/components/home/StatusSection";
 import EmployerNetwork from "@/components/home/EmployerNetworkSection";
 import Footer from "@/components/home/Footer";
-import FAQSection from "@/components/home/FaqSection";
-import CaseStudySection from "@/components/home/CaseStudySection";
 
 export default function Home() {
   return (
@@ -23,10 +18,7 @@ export default function Home() {
         <SuccessTicker />
         <ComparisonSection />
         <EmployerNetwork />
-        <CaseStudySection />
-        <ProcessSection />
-        <ProgramsSection />
-        <FAQSection />
+
         <Footer />
       </main>
     </>
