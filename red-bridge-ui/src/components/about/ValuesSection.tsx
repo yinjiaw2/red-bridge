cc1a4947230850f39efb3a9f679338caab1bdf8f
nicole-nxn <nicolenxn1234@gmail.com>
2026-04-14 17:19:36 +1000

    fix value

--- red-bridge-ui/src/components/about/ValuesSection.tsx
+++ red-bridge-ui/src/components/about/ValuesSection.tsx
@@ -46,7 +46,7 @@ export default function ValuesSection() {
         </div>
 
         {/* Cards grid */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
           {items.map((item, i) => (
             <div
               key={i}
